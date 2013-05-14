e2dc3e89ad9719fe253610a8ce87a6eae1d904a2
mribeirodantas@lais.huol.ufrn.br <mribeirodantas@lais.huol.ufrn.br>
2013-05-14 19:26:53 -0300

    Some updates

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ sysaccount
 ==========
 
 System account manager
+
+Well, I'm doing it just for the sake of practicing
+Shell Script :)
--- sysaccount.sh
+++ sysaccount.sh
@@ -2,12 +2,23 @@
 
 # Variables
 attemptN=0
+#Account information
+ACCOUNT=""
+PASSWORD=""
+#Values informed
+LOGIN=""
+PASS=""
+
 
 # Functions
+function usage() {
+  echo "Usage: sysaccount [option] [argument]"
+}
+
 function login() {
   if !([ -s ~/.passwd -a -r ~/.passwd ]);
   then
-    echo "There is no passwd file."
+    echo "The account database is empty."
     echo "Would you like to add a new user?"
     read -n1 -p "[Y/n]" REPLY
     if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ];
@@ -25,8 +36,8 @@ function login() {
 
   read -p "Login: " LOGIN
   read -s -p "Password: " PASS
-  ACCOUNT=`grep "$LOGIN" ~/.passwd | awk -F " " {' print $1 '}`
-  PASSWORD=`grep "$LOGIN" ~/.passwd | awk -F " " {' print $2 '}`
+  ACCOUNT=`grep "$LOGIN" ~/.passwd | awk -F ":" {' print $1 '}`
+  PASSWORD=`grep "$LOGIN" ~/.passwd | awk -F ":" {' print $2 '}`
   PASS=`echo "$PASS" | shasum | awk -F " " {' print $1 '}`
 
   if [ "$LOGIN" == "$ACCOUNT" ] && [ "$PASS" == "$PASSWORD" ];
@@ -56,9 +67,9 @@ function logged() {
 function useradd() {
   if [ "$1" == "" ];
   then
-    read -p "Username: " USERNAME
+    read -p "Username: " LOGIN
   else
-    USERNAME=$1
+    LOGIN=$1
   fi
   read -s -p "Password: " PASSWORD
   #Newline
@@ -66,9 +77,9 @@ function useradd() {
   read -s -p "Password: " PASSWORD2
   if [ "$PASSWORD" == "$PASSWORD2" ];
   then
-    echo "$USERNAME `echo $PASSWORD | shasum`" >> ~/.passwd &&
-    echo -e "\nUser $USERNAME was added successfully." ||
-    echo -e "\nUser $USERNAME was not added."
+    echo "$LOGIN:`echo $PASSWORD | shasum | awk -F " " {' print $1 '}`" >> ~/.passwd &&
+    echo -e "\nUser $LOGIN was added successfully." ||
+    echo -e "\nUser $LOGIN was not added."
   else
     echo -e "\nPasswords didn't match."
     echo "Try again!"
@@ -77,12 +88,16 @@ function useradd() {
 }
 
 function userdel() {
+  if !([ -s ~/.passwd ]);
+  then
+    login
+  fi
   if [ "`grep "$1" ~/.passwd`" != "" ];
   then
     echo "`cat ~/.passwd | sed "/$1/ d"`" > ~/.passwd &&
     echo "Useraccount $1 successfully removed." ||
     echo "Useraccount $1 was not removed."
-    if [ "`cat ~/.passwd | grep " "`" == "" ];
+    if [ "`cat ~/.passwd | grep ":"`" == "" ];
     then
       rm ~/.passwd
     fi
@@ -94,16 +109,22 @@ function userdel() {
 # Main
 if [ "$#" == 0 ];
 then
-  echo "Usage: sysaccount [option] [argument]"
+  usage
 else
-  if [ "$1" == "-a" ] || [ "$1" == "--add" ];
-  then
-    useradd $2
-  elif [ "$1" == "-d" ] || [ "$1" == "--del" ];
-  then
-    userdel $2
-  elif [ "$1" == "-l" ] || [ "$1" == "--login" ];
-  then
-    login
-  fi
+  case "$1" in
+    -a|--add)
+      useradd $2
+      ;;
+    -d|--del)
+      if [ "$2" == "" ];
+      then
+        usage
+      else
+        userdel $2
+      fi
+      ;;
+    -l|--login)
+      login
+      ;;
+  esac
 fi
